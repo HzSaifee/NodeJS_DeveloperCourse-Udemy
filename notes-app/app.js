2a5c600c8e70321825474d9016189056f1204003
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-15 17:07:36 -0600

    Using npm modules in the Project

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,19 @@
-const validator = require('validator');
+const chalk = require('chalk');
+// const validator = require('validator');
 const getNotes = require('./notes.js');
+// // const add = require('./utils.js');
+// // // const fs = require('fs');
 
-// const add = require('./utils.js');
+// // // // fs.writeFileSync('notes.txt', 'Andrew asked me to change this Message!');
+// // // fs.appendFileSync('notes.txt', '\nAndrew made me add this Message!');
 
-// // const fs = require('fs');
-
-// // // fs.writeFileSync('notes.txt', 'Andrew asked me to change this Message!');
-// // fs.appendFileSync('notes.txt', '\nAndrew made me add this Message!');
-
-// const sum = add(-2, 4);
-
-// console.log(sum);
+// // const sum = add(-2, 4);
+// // console.log(sum);
 
 const msg = getNotes();
-
 console.log(msg);
 
-console.log(validator.isEmail('hzsaifee52@gmail.com'));
+// console.log(validator.isEmail('hzsaifee52@gmail.com'));
+
+console.log(chalk.green('Success!'));
+console.log(chalk.green.bold.inverse('Success!'));
