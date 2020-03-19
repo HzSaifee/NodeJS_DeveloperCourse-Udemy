08434ef1ce69931b249dc66afc4e370b25af2d98
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-19 01:51:28 -0600

    Working with a new npm Package yargs to Parse the console inputs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
+const yargs = require('yargs');
 const chalk = require('chalk');
 // const validator = require('validator');
 const getNotes = require('./notes.js');
@@ -10,10 +11,58 @@ const getNotes = require('./notes.js');
 // // const sum = add(-2, 4);
 // // console.log(sum);
 
-const msg = getNotes();
-console.log(msg);
+// const msg = getNotes();
+// console.log(msg);
 
-// console.log(validator.isEmail('hzsaifee52@gmail.com'));
+// // console.log(validator.isEmail('hzsaifee52@gmail.com'));
 
-console.log(chalk.green('Success!'));
-console.log(chalk.green.bold.inverse('Success!'));
+// console.log(chalk.red.bold('Error!'));
+// console.log(chalk.green.bold.inverse('Success!'));
+
+// const command = process.argv[2];
+
+// if(command === 'add'){
+//     console.log('Adding Note!');
+// }
+
+// else if(command === 'remove'){
+//     console.log('Removing Note!');
+// }
+
+// Create Add Command
+yargs.command({
+    command: 'add',
+    describe: 'Adding a new Note',
+    handler: function(){
+        console.log('Adding a New Note!')
+    }
+})
+
+// Create Remove Command
+yargs.command({
+    command: 'remove',
+    describe: 'Removing a Note',
+    handler: function(){
+        console.log('Removing a Note!')
+    }
+})
+
+// Create read Command
+yargs.command({
+    command: 'read',
+    describe: 'Reading a Note',
+    handler: function(){
+        console.log('Reading a Note!')
+    }
+})
+
+// Create List Command
+yargs.command({
+    command: 'list',
+    describe: 'Listing a Note',
+    handler: function(){
+        console.log('Listing a Note!')
+    }
+})
+
+console.log(yargs.argv);
