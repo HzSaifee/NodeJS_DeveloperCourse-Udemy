0d07ced70f6cc5aa0e7c9a9e8771a2763a2dc702
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-25 02:52:32 -0600

    Updated My Excercise: Web Server Application Started

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,53 @@
 const express = require ('express');
 const path = require ('path');
 
-const publicDirectoryPath = path.join(__dirname, '../public');
-
 const app = express();
+const port = 3001;
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Huzefa Saifee'
+    });
 })
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Huzefa Saifee'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Help Provided!'
+    });
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: "Salt Lake City",
+        latitude: -114.5,
+        longitude: 45,
+        forecast: "Currently its 13 degree celcius."
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on Port 3001.')
+})
+
 
 // app.get('', (req, res) => {
 //     res.send('<h1>Weather Application</h1>')
@@ -26,16 +63,3 @@ app.get('', (req, res) => {
 // app.get('/about', (req, res) => {
 //     res.send('<h2>About Page</h2>')
 // })
-
-app.get('/weather', (req, res) => {
-    res.send({
-        location: "Salt Lake City",
-        latitude: -114.5,
-        longitude: 45,
-        forecast: "Currently its 13 degree celcius."
-    })
-})
-
-app.listen(3001, () => {
-    console.log('Server is up on Port 3001.')
-})
