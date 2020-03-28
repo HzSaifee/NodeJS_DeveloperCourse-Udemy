acf2a1ee8e4b1c17fdc696699ed65f5b6476ac78
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-28 17:56:32 -0600

    Weather Application with Website Integration completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,21 @@
 const express = require ('express');
 const path = require ('path');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 const port = 3001;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewPath = path.join(__dirname, '../templates');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -31,21 +36,69 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'Help Provided!'
+        helpText: 'Help Provided!',
+        title: 'Help',
+        name: 'Huzefa Saifee'
     });
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
     res.send({
-        location: "Salt Lake City",
-        latitude: -114.5,
-        longitude: 45,
-        forecast: "Currently its 13 degree celcius."
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Huzefa Saifee',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Huzefa Saifee',
+        errorMessage: 'Page not found.'
     })
 })
 
 app.listen(port, () => {
-    console.log('Server is up on Port 3001.')
+    console.log('Server is up on Port',port,'.')
 })
 
 
