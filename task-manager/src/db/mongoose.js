3d3ebdd843478e29b46cfd84356c64eaf239dbbc
Huzefa Saifee <hzsaifee52@gmail.com>
2020-04-08 18:02:53 -0600

    Learnt about Mongoose Validation & Sanitization

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,15 +8,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value){
+          if(value.toLowerCase().includes("password")){
+              throw new Error('Password cannot contain string "password"');
+          }
+      }  
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a positive number');
+            }
+        }
     }
 });
 
 // const me = new User({
-//     name: 'Huzefa',
+//     name: '     Huzefa ',
+//     email: 'hzsaifEE52@gmail.com     ',
+//     password: 'huzefa',
 //     age: 23
 // });
 
@@ -27,10 +60,13 @@ const User = mongoose.model('User', {
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 });
 
