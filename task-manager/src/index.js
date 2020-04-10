008a3675f26460f992ef3117435715400b54c991
Huzefa Saifee <hzsaifee52@gmail.com>
2020-04-10 00:02:52 -0600

    Added code to logout succefully

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,19 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3001;
 
+// app.use((req, res, next) => {
+//     // console.log(req.method, req.path);
+//     if(req.method == 'GET'){
+//         res.send('Sorry! GET Requests are currently disabled! Try Again Later');
+//     }else{
+//         next();
+//     }
+// });
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Sorry! The site is currently disabled! Try Again Later, After the Maintainance is done.');
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -32,14 +33,42 @@ router.post('/users/login', async(req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async (req, res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        })
 
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch(error){
+        await req.user.save();
+
+        res.send();
+    }catch(error){
         res.status(500).send();
     }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try{
+        req.user.tokens = [];
+
+        await req.user.save();
+
+        res.send();
+    }catch(error){
+        res.status(500).send();
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+
+    res.send(req.user);
+
+    // try {
+    //     const users = await User.find({});
+    //     res.send(users);
+    // } catch(error){
+    //     res.status(500).send();
+    // }
 
     // User.find({}).then((users) => {
     //     res.send(users);
