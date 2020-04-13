7f76b174e39b377ede9895fd8e1460a684e0bec6
Huzefa Saifee <hzsaifee52@gmail.com>
2020-04-13 00:17:27 -0600

    Finished code for task manager, with added feature of sending emails to the user

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+const { sendWelcomeEmail, sendDeleteEmail } = require('../emails/account');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -10,6 +11,7 @@ router.post('/users', async (req, res) => {
 
     try{
         await user.save();
+        sendWelcomeEmail(user.email, user.name);
         const token = await user.generateAuthToken();
         
         res.status(201).send({ user, token });
@@ -142,6 +144,8 @@ router.delete('/users/me',  auth, async(req, res) => {
         // // if(!user){
         // //     res.status(404).send();
         // // }
+
+        sendDeleteEmail(req.user.email, req.user.name);
         await req.user.remove();
         res.send(req.user);
     }catch(error){
