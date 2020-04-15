7acffe73c106cac02035385b466f43f98d3d1513
Huzefa Saifee <hzsaifee52@gmail.com>
2020-04-14 19:06:34 -0600

    Learning about Mocking Test Environments

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -30,11 +30,35 @@ test('Should signup a new user', async () => {
     }).expect(201);
 })
 
+test('Should signup a new user', async () => {
+    const response = await request(app).post('/users').send({
+        name: 'Hz Saifee',
+        email: 'huzusaifee@gmail.com',
+        password: 'JaiHo123'
+    }).expect(201)
+
+    // Assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Hz Saifee',
+            email: 'huzusaifee@gmail.com',
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('JaiHo123')
+})
+
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
-    }).expect(200);
+    }).expect(200)
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login nonexistent user', async () => {
@@ -65,6 +89,8 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Should not delete account for unauthenticate user', async () => {
